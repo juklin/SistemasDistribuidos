2475a913a9e1fda215ad621f8841f7cbf36023d3
Juliana Pontes <julianamateus@alu.ufc.br>
2019-10-30 21:32:59 -0300

    Create client.java

---
+++ TrabalhoFinal/client.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class client {
+ public static void main(String[] args) 
+                 throws UnknownHostException, IOException {
+     Socket cliente = new Socket("", 7000);
+     System.out.println("O cliente se conectou ao servidor!");
+
+     Scanner teclado = new Scanner(System.in);
+     PrintStream saida = new PrintStream(cliente.getOutputStream());
+
+     while (teclado.hasNextLine()) {
+         saida.println(teclado.nextLine());
+     }
+
+     saida.close();
+     teclado.close();
+     cliente.close();
+ }
+}
